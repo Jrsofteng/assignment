b9fdd2080ac98abb608a641621f42ac4d9263074
Jrsofteng <bsoda945@gmail.com>
2024-03-11 09:13:58 +0100

    first commit

---
+++ assignment.js
@@ -0,0 +1,23 @@
+// a code to compute the score of students in a class
+//the code is written below
+
+
+let score = 78
+
+if (score >=70 && score <100) {
+    console.log("A")
+    
+} else if (score <70 && score >=60){
+    console.log("B")
+}else if (score <60 && score >=50){
+    console.log("C")
+}else if(score <50 && score >=45){
+    console.log("D")
+}else if(score <45 && score >=40){
+    console.log("E")
+}else if(score <40){
+    console.log("F")
+}else{
+    console.log("score does not exist in this result")
+}
+    
